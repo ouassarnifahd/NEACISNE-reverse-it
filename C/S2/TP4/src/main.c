ca05b0de1fe1ef01d1e785a745e1e577eb9304ed
OUASSARNI Fahd <ouassarnifahd@hotmail.com>
2017-05-08 05:09:44 +0200

    modified:   src/main.c

--- C/S2/TP4/src/main.c
+++ C/S2/TP4/src/main.c
@@ -13,18 +13,25 @@ int main(int argc, char const *argv[]){
     #elif ELEM_REAL
     TElement min = FLT_MIN, max = FLT_MAX;
     #endif
+    printf("Initialisation du tableau aléatoire:\n");
     PTElement tabElts = initRandomTab(ARRAY_SIZE,&min,&max,sizeof(TElement),TElement_Random);
     displayTab(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Display);
     #ifdef SELECTION
+    printf("tri du tableau avec tri selection.\n");
     selection_sort(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
     #elif INSERTION
+    printf("tri du tableau avec tri insertion.\n")
     insertion_sort(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
-    #elif BUBBLE
+    #elif BUBBLE;
+    printf("tri du tableau avec tri bulles.\n");
     bubble_sort(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
     #elif MERGE
+    printf("tri du tableau avec tri fusion.\n");
     merge_sort(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
     #elif QUICK
+    printf("tri du tableau avec tri rapide.\n");
     quick_sort(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
     #endif
+    printf("Affichage du tableau trié.\n");
     displayTab(tabElts,ARRAY_SIZE,sizeof(TElement),TElement_Display);
 }
