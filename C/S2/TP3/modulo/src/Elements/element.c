a673d89199097f2fba5be2aceff6aa4bba6173cd
OUASSARNI Fahd <ouassarnifahd@hotmail.com>
2017-05-11 15:37:08 +0200

    new file:   TP3/modulo/Doxyfile 	new file:   TP3/modulo/inc/Elements/element.h 	new file:   TP3/modulo/inc/queue.h 	new file:   TP3/modulo/makefile 	new file:   TP3/modulo/src/Elements/element.c 	new file:   TP3/modulo/src/main.c 	new file:   TP3/modulo/src/queue.c

---
+++ C/S2/TP3/modulo/src/Elements/element.c
@@ -0,0 +1,29 @@
+/** @file element.c
+*   @brief Ficher source contenant les fonctions de l'elements.
+*/
+
+#include"../../inc/Elements/element.h"
+
+#ifdef _FLOAT_
+
+void display_element(const void *pElement){
+    float flottant= *(const float *)pElement;
+    printf("%.2f", flottant);
+}
+
+bool read_element(const void *pElement){
+    return !scanf("%f",(float *)pElement);
+}
+
+#elif _INT_
+
+void display_element(const void *pElement){
+    int entier= *(const int *)pElement;
+    printf("%d", entier);
+}
+
+bool read_element(const void *pElement){
+    return !scanf("%d",(int *)pElement);
+}
+
+#endif
