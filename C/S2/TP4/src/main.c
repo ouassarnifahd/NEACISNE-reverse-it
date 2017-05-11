cf453c12c2f9757d43f8afebf1d161489f9ffef9
OUASSARNI Fahd <ouassarnifahd@hotmail.com>
2017-05-11 11:59:18 +0200

    modified:   makefile 	modified:   src/main.c

--- C/S2/TP4/src/main.c
+++ C/S2/TP4/src/main.c
@@ -11,34 +11,33 @@ int main(int argc, char const *argv[]){
     srand48(time(NULL));
     #endif
     PTElement tabElems = initRandomTab(ARRAY_SIZE, &ElementMin, &ElementMax, sizeof(TElement), TElement_Random);
+    displayTab(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Display);
     if (is_sorted(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare)) {
         printf("Le tableau est deja trié!\n");
-        displayTab(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Display);
     } else {
         printf("Le tableau n'est pas trié!\n");
-        displayTab(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Display);
         #ifdef SELECTION
-        printf("tri du tableau avec tri selection.\n");
+        printf("Tri du tableau avec le tri selection.\n");
         selection_sort(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
         #elif INSERTION
-        printf("tri du tableau avec tri insertion.\n");
+        printf("Tri du tableau avec le tri insertion.\n");
         insertion_sort(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
         #elif BUBBLE
-        printf("tri du tableau avec tri bulles.\n");
+        printf("Tri du tableau avec le tri bulles.\n");
         bubble_sort(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
         #elif MERGE
-        printf("tri du tableau avec tri fusion.\n");
+        printf("Tri du tableau avec le tri fusion.\n");
         merge_sort(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
         #elif QUICK
-        printf("tri du tableau avec tri rapide.\n");
+        printf("Tri du tableau avec le tri rapide.\n");
         quick_sort(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare);
         #endif
+        displayTab(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Display);
         if (is_sorted(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Compare)) {
             printf("Le tableau a été trié!\n");
         } else {
             printf("Le tableau n'a pas été trié!\n");
         }
-        displayTab(tabElems,ARRAY_SIZE,sizeof(TElement),TElement_Display);
     }
     free(tabElems);
 }
