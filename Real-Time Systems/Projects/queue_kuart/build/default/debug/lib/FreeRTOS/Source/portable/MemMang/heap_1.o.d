aaf12763d5d369ea52c0c7d23d9210fe398554b4
OUASSARNI Fahd <ouassarnifahd@hotmail.com>
2019-09-24 14:03:14 +0200

    new files

---
+++ Real-Time Systems/Projects/queue_kuart/build/default/debug/lib/FreeRTOS/Source/portable/MemMang/heap_1.o.d
@@ -0,0 +1,10 @@
+build/default/debug/lib/FreeRTOS/Source/portable/MemMang/heap_1.o:  \
+ lib/FreeRTOS/Source/portable/MemMang/heap_1.c  \
+ lib/FreeRTOS/Source/include/FreeRTOS.h  \
+ lib/FreeRTOS/Source/include/projdefs.h  \
+inc/FreeRTOSConfig.h  \
+ lib/FreeRTOS/Source/include/portable.h  \
+ lib/FreeRTOS/Source/portable/MPLAB/PIC32MX/portmacro.h  \
+ lib/FreeRTOS/Source/include/mpu_wrappers.h  \
+ lib/FreeRTOS/Source/include/task.h  \
+lib/FreeRTOS/Source/include/list.h 
---
+++ Real-Time Systems/Projects/queue_kuart/build/default/debug/lib/peripherals/kuart/pic32maxiweb/src/kuart.o.d
@@ -0,0 +1,11 @@
+build/default/debug/lib/peripherals/kuart/pic32maxiweb/src/kuart.o:  \
+ lib/peripherals/kuart/pic32maxiweb/src/kuart.c  \
+ lib/peripherals/kuart/pic32maxiweb/h/kuart.h  \
+ lib/FreeRTOS/Source/include/FreeRTOS.h  \
+ lib/FreeRTOS/Source/include/projdefs.h  \
+inc/FreeRTOSConfig.h  \
+ lib/FreeRTOS/Source/include/portable.h  \
+ lib/FreeRTOS/Source/portable/MPLAB/PIC32MX/portmacro.h  \
+ lib/FreeRTOS/Source/include/mpu_wrappers.h  \
+ lib/FreeRTOS/Source/include/semphr.h  \
+lib/FreeRTOS/Source/include/queue.h 
